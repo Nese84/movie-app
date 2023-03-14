60ae39029e92fcd543c84c2429308673b0917477
Nese84 <nbserbest84@gmail.com>
2023-03-14 17:05:30 +0300

    firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         data-te-navbar-ref=""
       >
         <div className="flex w-full flex-wrap items-center justify-between px-6">
-          <Link className="pr-2 text-xl font-semibold text-white" to="/">
+          <Link className="pr-2 text-2xl font-semibold text-white" to="/">
             React Movie App
           </Link>
 
@@ -44,13 +44,13 @@ const Navbar = () => {
                 data-te-dropdown-menu-ref=""
               >
                 <li>
-                  <a
+                  <Link
                     className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
                     href="#"
                     data-te-dropdown-item-ref=""
                   >
                     Action
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a
